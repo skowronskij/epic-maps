23b7c106ef93ba8755be312cc5b89d0a7ef40308
Mativane <osko1996@gmail.com>
2020-05-08 20:03:03 +0200

    stylePolygonForest, stylePolygonMountains ; as well as base_style functions: restylePolygon() and polygon2markers()

--- styles/base_style.py
+++ styles/base_style.py
@@ -26,7 +26,7 @@ class BaseStyle:
     def generate_layout(self):
         pass
 
-    def recolorPolygon(self, vectorLayer, minRed, maxRed, minGreen, maxGreen, minBlue, maxBlue, outlineWidth):
+    def restylePolygon(self, vectorLayer, minRed, maxRed, minGreen, maxGreen, minBlue, maxBlue, outlineWidth, make_boundary_waves=False):
         red = random.randrange(minRed, maxRed)
         green = random.randrange(minGreen, maxGreen)
         blue = random.randrange(minBlue, maxBlue)
@@ -40,3 +40,58 @@ class BaseStyle:
 
         vectorLayer.renderer().setSymbol(symbol)
         vectorLayer.triggerRepaint()
+        
+        if make_boundary_waves:
+            registry = QgsProject.instance()
+
+            vectorLayer.selectAll()
+            bb = vectorLayer.boundingBoxOfSelected()
+            vectorLayer.removeSelection()
+            mean = (bb.height() + bb.width()) / 2
+            bufDist = int(mean / 100)
+            bufDist2 = bufDist * 2
+
+            buffs = []
+            buffs.append(processing.run("qgis:buffer", {'INPUT':vectorLayer, 'DISTANCE': bufDist, 'DISSOLVE': True, 'SEGMENTS': 100, 'OUTPUT':'memory:'})['OUTPUT'])
+            buffs.append(processing.run("qgis:buffer", {'INPUT':vectorLayer, 'DISTANCE': bufDist2, 'DISSOLVE': True, 'SEGMENTS': 100, 'OUTPUT':'memory:'})['OUTPUT'])
+            
+            dashes = ['5;2', '3;1']
+            for buff, dash in zip(buffs, dashes):
+                lines = processing.run("qgis:polygonstolines", {'INPUT':buff, 'OUTPUT':'memory:'})['OUTPUT']
+                
+                lines_symbol = QgsLineSymbol.createSimple({'outline_color': border_colors, \
+                    'outline_width':'0.2', 'joinstyle':'round', \
+                    'use_custom_dash': '1', 'customdash': dash})
+                lines.renderer().setSymbol(lines_symbol)
+                registry.addMapLayer(lines)
+
+    def polygon2markers(self, vectorLayer, epsg, marker_filename):
+        provider = "memory"
+        layerSource = 'Point?crs=' + epsg
+        pLayer = QgsVectorLayer(layerSource, "markers", provider)
+
+        features = vectorLayer.getFeatures()
+        for feature in features:
+            polygon = feature.geometry()
+            extend = polygon.boundingBox()
+
+            number_of_points = 50 #póki co przypadkowa wartość, trzeba to jeszcze przemyśleć
+
+            while True:
+                x = random.uniform(extend.xMinimum(), extend.xMaximum())
+                y = random.uniform(extend.yMinimum(), extend.yMaximum())
+                point = QgsPointXY(x,y)
+                geopoint = QgsGeometry.fromPointXY(point)
+                if geopoint.within(polygon):
+                    feature = QgsFeature()
+                    feature.setGeometry(geopoint)
+                    pLayer.dataProvider().addFeature(feature)
+                    number_of_points -= 1
+                if number_of_points <= 0:
+                    break
+        pLayer.updateExtents()
+        registry.addMapLayer(pLayer)
+
+        symbol = QgsSvgMarkerSymbolLayer(marker_filename)
+        symbol.setSize(4) #póki co przypadkowa wartość, trzeba to jeszcze przemyśleć
+        pLayer.renderer().symbol().changeSymbolLayer(0, symbol )
--- styles/fantasy/fantasy_style.py
+++ styles/fantasy/fantasy_style.py
@@ -13,36 +13,22 @@ class FantasyStyle(BaseStyle):
 
     #Style - do przetestowania!
     def stylePolygonLands(self, vectorLayer):        
-        self.recolorPolygon(vectorLayer, 250, 255, 230, 250, 160, 200, 0.2)
-
-        registry = QgsProject.instance()
-
-        vectorLayer.selectAll()
-        bb = vectorLayer.boundingBoxOfSelected()
-        vectorLayer.removeSelection()
-        mean = (bb.height() + bb.width()) / 2
-        bufDist = int(mean / 100)
-        bufDist2 = bufDist * 2
-
-        buffs = []
-        buffs.append(processing.run("qgis:buffer", {'INPUT':vectorLayer, 'DISTANCE': bufDist, 'DISSOLVE': True, 'SEGMENTS': 100, 'OUTPUT':'memory:'})['OUTPUT'])
-        buffs.append(processing.run("qgis:buffer", {'INPUT':vectorLayer, 'DISTANCE': bufDist2, 'DISSOLVE': True, 'SEGMENTS': 100, 'OUTPUT':'memory:'})['OUTPUT'])
-        
-        dashes = ['5;2', '3;1']
-        for buff, dash in zip(buffs, dashes):
-            lines = processing.run("qgis:polygonstolines", {'INPUT':buff, 'OUTPUT':'memory:'})['OUTPUT']
-            
-            lines_symbol = QgsLineSymbol.createSimple({'outline_color': border_colors, \
-                'outline_width':'0.2', 'joinstyle':'round', \
-                'use_custom_dash': '1', 'customdash': dash})
-            lines.renderer().setSymbol(lines_symbol)
-            registry.addMapLayer(lines)
+        self.restylePolygon(vectorLayer, 250, 255, 230, 250, 160, 200, 0.2, True)
     
     def stylePolygonWater(self, vectorLayer):
-        self.recolorPolygon(vectorLayer, 100, 180, 200, 230, 245, 250, 0.15)
+        self.restylePolygon(vectorLayer, 100, 180, 200, 230, 245, 250, 0.15)
 
     def stylePolygonOther(self, vectorLayer):
-        self.recolorPolygon(vectorLayer, 240, 255, 190, 220, 100, 140, 0.15)
+        self.restylePolygon(vectorLayer, 240, 255, 190, 220, 100, 140, 0.15)
+
+    def stylePolygonForest(self, vectorLayer):
+        epsg = vectorLayer.crs().authid()
+        self.polygon2markers(vectorLayer, epsg, ":/fantasy/resources/tree.svg")
+
+    def stylePolygonMountains(self, vectorLayer):
+        epsg = vectorLayer.crs().authid()
+        self.polygon2markers(vectorLayer, epsg, ":/fantasy/resources/mountain.svg")
+
 
     def stylePointTowns(self,vectorLayer):
         self.symbol = QgsSvgMarkerSymbolLayer(":/fantasy/resources/medieval.svg")
